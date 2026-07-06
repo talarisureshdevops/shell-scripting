b094c6d06fc33a914fccfe5f0dfa25fe046ccf0c
Talari <suresh.talari-ext@hpe.com>
2026-07-06 15:54:21 +0530

    added new file here

---
+++ 06-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter your username::"
+read user_name
+
+echo "user name is $user_name"
+
+echo "please enter your password::"
+read -s password
+
+echo "user password is $password"
+
