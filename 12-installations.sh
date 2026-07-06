84c37cd34f5971a2197af81142bef470fed9fce0
Talari <suresh.talari-ext@hpe.com>
2026-07-06 21:10:26 +0530

    added new file

--- 12-installations.sh
+++ 12-installations.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-USER_ID=$(id - u)
-if [ $USER_ID - ne 0 ]; then
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then
    echo "Please run this script with root user access"
    exit 1
 fi
 echo "installing nginx"
 dnf install nginx -y
-if [ $? - ne 0 ]; then
+if [ $? -ne 0 ]; then
    echo "Installing nginx........FAILURE"
    exit 1
 else
