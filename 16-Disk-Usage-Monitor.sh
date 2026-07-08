90d4e7c3ef279d4c56a8753d429af5b539338626
Talari <suresh.talari-ext@hpe.com>
2026-07-08 14:53:09 +0530

    added new one

--- 16-Disk-Usage-Monitor.sh
+++ 16-Disk-Usage-Monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 threshold=80
 
-df -h | aws 'NR>1 {print $5,$6}' | while read output
+df -h | awk 'NR>1 {print $5,$6}' | while read output
 do
-usage=$(echo $output | aws '{print $1}' | cut -d'%' -f1)
-partition=$(echo $output | awk '{print $2}')
+usage=$(echo "$output" | awk '{print $1}' | cut -d'%' -f1)
+partition=$(echo "$output" | awk '{print $2}')
 
 if [ $usage -ge threshold ]; then
    echo "warning $partition is ${usage} % full"
