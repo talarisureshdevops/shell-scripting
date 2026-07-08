9cdfb308c51e13691f43cbe75dbcd441d1bff477
Talari <suresh.talari-ext@hpe.com>
2026-07-08 17:46:35 +0530

    added new one

---
+++ 18-Disk-monitor.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# disk usage monitoring report
+threshold=80
+LOGFILE="/tmp/disk_monitoring.log"
+echo
+echo "==============================================="
+echo "Disk Usage Monitoring Report"
+echo "Date : $(date)"
+df -h | aws "NR>1 {print $5,$6}" | while read usage partition
+do
+
+usage=${usage%\%}
+if [ "$usage" -ge "$threshold" ]; then 
+   echo "[Warning] $parition is ${usage}% full"
+   echo "$(date): warning $partition is ${usage}% full" >>$LOGFILE
+else
+   echo "[OK] $partition is ${usage}% used"
+fi
+echo
+echo "log_file: $LOGFILE"
