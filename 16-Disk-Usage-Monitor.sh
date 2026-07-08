d639e5a5532cf0463aa1963ecc4b3c86064df279
Talari <suresh.talari-ext@hpe.com>
2026-07-08 14:59:47 +0530

    added new one

--- 16-Disk-Usage-Monitor.sh
+++ 16-Disk-Usage-Monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 threshold=30
+RED='\033[0;31m'
+NC='\033[0m'   # No Color (Reset)
 
 df -h | awk 'NR>1 {print $5,$6}' | while read output
 do
@@ -7,7 +9,7 @@ usage=$(echo "$output" | awk '{print $1}' | cut -d'%' -f1)
 partition=$(echo "$output" | awk '{print $2}')
 
 if [ "$usage" -ge "$threshold" ]; then
-   echo "warning $partition is ${usage} % full"
+   echo -e "${RED}WARNING: $partition is ${usage}% full${NC}"
 fi
 done
 
