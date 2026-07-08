78744b2ff773fe20288ef64f58cf1b8091fa96a7
Talari <suresh.talari-ext@hpe.com>
2026-07-08 12:29:33 +0530

    added new one

---
+++ 15-case.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "please enter the fruit name here::" fruits
+case "$fruits" in 
+     apple|Apple)
+       echo "it's a apple" ;;
+     banana)
+       echo "it's a banana" ;;
+    cherry|grape)
+       echo "cherry or graph" ;;
+    *)
+       echo "unknown fruit";;
+    esac
